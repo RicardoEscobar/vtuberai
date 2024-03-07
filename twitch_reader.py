96cf12efc19abe86f3dff1f9b2ae53d4c2edc5f2
Jorge Ricardo Escobar Carrasco <jorge.ricardo.escobar@gmail.com>
2024-03-06 20:39:30 -0600

    Add Twitch chat message saving functionality

--- twitch_reader.py
+++ twitch_reader.py
@@ -1,6 +1,8 @@
 """This module is used to read the twitch chat and send the messages to the vtuber twitch channel."""
+
 import os
 import asyncio
+import datetime
 
 from twitchAPI.twitch import Twitch
 from twitchAPI.helper import first
@@ -9,13 +11,16 @@ from twitchAPI.type import AuthScope, ChatEvent
 from twitchAPI.chat import Chat, EventData, ChatMessage, ChatSub, ChatCommand
 from dotenv import load_dotenv
 
+from json_helper import add_to_json
+
 
 # load the environment variables
 load_dotenv()
 APP_ID = os.getenv("TWITCH_APP_ID")
 APP_SECRET = os.getenv("TWITCH_APP_SECRET")
 USER_SCOPE = [AuthScope.CHAT_READ, AuthScope.CHAT_EDIT]
-TARGET_CHANNEL = 'RicardoEscobar'
+TARGET_CHANNEL = "RicardoEscobar"
+
 
 async def twitch_example():
     # initialize the twitch instance, this will by default also create a app authentication for you TWITCH_APP_ID and TWITCH_APP_SECRET
@@ -27,6 +32,84 @@ async def twitch_example():
     user = await first(twitch.get_users(logins=TARGET_CHANNEL))
     # print the ID of your user or do whatever else you want with it
     print(user.id)
+
+
+# this will be called when the event READY is triggered, which will be on bot start
+async def on_ready(ready_event: EventData):
+    print("Bot is ready for work, joining channels")
+    # join our target channel, if you want to join multiple, either call join for each individually
+    # or even better pass a list of channels as the argument
+    await ready_event.chat.join_room(TARGET_CHANNEL)
+    # you can do other bot initialization things in here
+
+
+# this will be called whenever a message in a channel was send by either the bot OR another user
+async def on_message(msg: ChatMessage):
+    print(f"in {msg.room.name}, {msg.user.name} said: {msg.text}")
+    # save the message to a JSON file
+    data = {
+        "timestamp": datetime.datetime.now().isoformat(),
+        "user": msg.user.name,
+        "message": msg.text,
+    }
+    add_to_json(data, f"{msg.room.name}.json")
+
+
+# this will be called whenever someone subscribes to a channel
+async def on_sub(sub: ChatSub):
+    print(
+        f"New subscription in {sub.room.name}:\\n"
+        f"  Type: {sub.sub_plan}\\n"
+        f"  Message: {sub.sub_message}"
+    )
+
+
+# this will be called whenever the !reply command is issued
+async def test_command(cmd: ChatCommand):
+    if len(cmd.parameter) == 0:
+        await cmd.reply("you did not tell me what to reply with")
+    else:
+        await cmd.reply(f"{cmd.user.name}: {cmd.parameter}")
+
+
+# this is where we set up the bot
+async def run():
+    # set up twitch api instance and add user authentication with some scopes
+    twitch = await Twitch(APP_ID, APP_SECRET)
+    auth = UserAuthenticator(twitch, USER_SCOPE)
+    token, refresh_token = await auth.authenticate()
+    await twitch.set_user_authentication(token, USER_SCOPE, refresh_token)
+
+    # create chat instance
+    chat = await Chat(twitch)
+
+    # register the handlers for the events you want
+
+    # listen to when the bot is done starting up and ready to join channels
+    chat.register_event(ChatEvent.READY, on_ready)
+    # listen to chat messages
+    chat.register_event(ChatEvent.MESSAGE, on_message)
+    # listen to channel subscriptions
+    chat.register_event(ChatEvent.SUB, on_sub)
+    # there are more events, you can view them all in this documentation
+
+    # you can directly register commands and their handlers, this will register the !reply command
+    chat.register_command("reply", test_command)
+
+    # we are done with our setup, lets start this bot up!
+    chat.start()
+
+    # lets run till we press enter in the console
+    try:
+        input("press ENTER to stop\n")
+    finally:
+        # now we can close the chat bot and the twitch api client
+        chat.stop()
+        await twitch.close()
+
+
+# lets run our setup
+asyncio.run(run())
 
 # run this example
 asyncio.run(twitch_example())
